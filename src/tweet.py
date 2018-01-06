dcd25e82c6c3e5eb8dd3e1d74bb19bae59d044fb
Kristrún Skúladóttir <krs65@hi.is>
2018-01-06 13:40:14 +0100

    making bot tweet daily at 1pm utc time

--- src/tweet.py
+++ src/tweet.py
@@ -4,7 +4,7 @@ from veg_recipe_bot import tweet
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('cron', hour=13, timezone=utc)
 def scheduled_job():
     tweet()
 
